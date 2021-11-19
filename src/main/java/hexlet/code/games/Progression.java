47311fef7b204b27a8a792f29e42cf59643db83d
Briankaiserx <Briankaiser@mail.ru>
2021-11-19 23:38:06 +0400

    update

--- src/main/java/hexlet/code/games/Progression.java
+++ src/main/java/hexlet/code/games/Progression.java
@@ -8,9 +8,10 @@ import static hexlet.code.Utils.generateRandom;
 
 public class Progression {
 
-    public static final int LENGTH_OF_PROGRESSION = 10;
+
     public static final String PROGRESSION = "Progression";
     static final String RULESGAME = "What number is missing in the progression?";
+    public static final int LENGTH_OF_PROGRESSION = 10;
 
     public static String generateQuestion(int[] array, int missingPos) {
         StringBuilder arrayStr = new StringBuilder();
@@ -26,7 +27,7 @@ public class Progression {
     }
 
 
-    public static int[] generateProgression(int step, int firstElement, int length) {
+    public static int[] getProgression(int step, int firstElement, int length) {
         int[] array = new int[length];
         array[0] = firstElement;
         for (int i = 1; i < length; i++) {
@@ -42,7 +43,7 @@ public class Progression {
         for (int i = 0; i < ROUNDS; i++) {
             int progressionStep = generateRandom(LENGTH_OF_PROGRESSION);
             int firstElem = generateRandom(LENGTH_OF_PROGRESSION);
-            int[] array = generateProgression(progressionStep, firstElem, LENGTH_OF_PROGRESSION);
+            int[] array = getProgression(progressionStep, firstElem, LENGTH_OF_PROGRESSION);
             int missingPos = generateRandom(LENGTH_OF_PROGRESSION - 1);
             questions[i] = generateQuestion(array, missingPos);
             answers[i] = String.valueOf(array[missingPos]);
